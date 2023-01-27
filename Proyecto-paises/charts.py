45f285b21c43573827cef64376dbc5de55125d2d
Jesus Caro <jdgarcia1@unisangil.edu.co>
2023-01-27 15:18:49 -0500

    Genera grafico circular de paises

---
+++ Proyecto-paises/charts.py
@@ -0,0 +1,22 @@
+#Importa la libreria de graficas
+# as permite tener un alias para llamarla de forma mas facil
+import matplotlib.pyplot as plt
+
+#Grafico de barras
+def generate_bar_chart(labels, values):
+  fig, ax = plt.subplots()
+  ax.bar(labels, values)
+  #Guarda una imagen en formato png en la carpeta graficas
+  plt.savefig('Proyecto-paises/barras.png')
+  #Cierra el archivo
+  plt.close()
+  
+#Grafica de circulos -- torta
+def generate_pie_chart(labels, values):
+  fig, ax = plt.subplots()
+  ax.pie(values, labels=labels)
+  ax.axis('equal')
+  #Guarda una imagen en formato png en la carpeta graficas
+  plt.savefig('Proyecto-paises/circulo.png')
+  #Cierra el archivo
+  plt.close()
