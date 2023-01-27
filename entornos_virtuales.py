f921cfd29e124c1315610b07c451fd10208f6ff1
Jesus Caro <jdgarcia1@unisangil.edu.co>
2023-01-27 15:54:52 -0500

    Se agrego comando que faltaba para crear entornos virtuales

--- entornos_virtuales.py
+++ entornos_virtuales.py
@@ -1,5 +1,8 @@
 #Activar entornos virtuales
 # Se debe ingresar a la carpeta del proyecto y crear el entorno virtual
+# python -m venv nombre_maquina_virtual
+
+# Activar el entorno virtual
 # env\Scripts\activate.bat
 
 #Desactivar entornos virtuales
