f3ef1d51af71bfbe00809e9739d4139589b4c4fd
Jesus Caro <jdgarcia1@unisangil.edu.co>
2023-01-30 09:32:52 -0500

    web server FastApi

--- web-server/main.py
+++ web-server/main.py
@@ -1,19 +1,22 @@
 import store
-#from fastapi import FastAPI
-#from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-#app = FastAPI()
+#Crear una instancia
+app = FastAPI()
 
-#@app.get('/')
-#def get_list():
-#    return [1,2,3,]
+#Decorador
+@app.get('/')
+#Funcion
+def get_list():
+    return [1,2,3,]
 
-#@app.get('/contact', response_class=HTMLResponse)
-#def get_list():
-#    return """
-#        <h1>Hola soy una pagina</h1>
-#        <p>soy un parrafo</p>
-#    
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+   return """
+   <h1>Hola soy una pagina</h1>
+   <p>soy un parrafo</p>
+   """
 #Llama la funcion store
 def run():
     store.get_categories()
