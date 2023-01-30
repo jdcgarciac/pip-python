686ab780ea100c40d74918911f8e698d877b74d6
Jesus Caro <jdgarcia1@unisangil.edu.co>
2023-01-30 08:50:05 -0500

    funcion principal

---
+++ web-server/main.py
@@ -0,0 +1,22 @@
+import store
+#from fastapi import FastAPI
+#from fastapi.responses import HTMLResponse
+
+#app = FastAPI()
+
+#@app.get('/')
+#def get_list():
+#    return [1,2,3,]
+
+#@app.get('/contact', response_class=HTMLResponse)
+#def get_list():
+#    return """
+#        <h1>Hola soy una pagina</h1>
+#        <p>soy un parrafo</p>
+#    
+#Llama la funcion store
+def run():
+    store.get_categories()
+#Que lo corra como scrip
+if __name__ == '__main__':
+    run()
